bfeb97de160f7184bef7b31203d8925c6a165ea0
DivyanshSareen <sareendivyansh@gmail.com>
2021-07-16 20:17:25 +0530

    added new portfolio boilerplate

---
+++ js/index.js
@@ -0,0 +1,12 @@
+  
+    document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems);
+  });
+  document.addEventListener('DOMContentLoaded', () => {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
+  var instance = M.Carousel.init({
+    indicators: true
+  });
